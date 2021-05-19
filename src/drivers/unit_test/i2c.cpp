4555ded34cd657e825843d18aaafeacb598fda4a
Phillip Johnston <phillip@embeddedartistry.com>
2021-05-19 13:29:03 -0700

    Improve unit test driver - actually write to tx and rx buffers in transfer.

--- src/drivers/unit_test/i2c.cpp
+++ src/drivers/unit_test/i2c.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-only OR Embedded Virtual Machine Commercial License
 
 #include "i2c.hpp"
+#include <cassert>
 
 using namespace test;
 
@@ -24,28 +25,53 @@ embvm::i2c::baud i2cTestDriver::baudrate_(embvm::i2c::baud baud) noexcept
 embvm::i2c::status i2cTestDriver::transfer_(const embvm::i2c::op_t& op,
 											const embvm::i2c::master::cb_t& cb) noexcept
 {
-	(void)cb;
-
-	bus_status_ = embvm::i2c::status::busy;
-
 	switch(op.op)
 	{
+		case embvm::i2c::operation::stop:
+		case embvm::i2c::operation::restart:
 		case embvm::i2c::operation::ping:
 			bus_status_ = embvm::i2c::status::ok;
 			break;
 		case embvm::i2c::operation::read:
-		case embvm::i2c::operation::write:
+			assert(op.rx_buffer);
+			assert(rxBuffer_.size() >= op.rx_size);
+			for(size_t i = 0; i < op.rx_size; i++)
+			{
+				op.rx_buffer[i] = rxBuffer_.front();
+				rxBuffer_.pop();
+			}
+			break;
 		case embvm::i2c::operation::writeRead:
-		case embvm::i2c::operation::stop:
-		case embvm::i2c::operation::restart:
+			assert(op.tx_buffer);
+			assert(op.tx_size > 0);
+			assert(op.rx_buffer);
+			assert(rxBuffer_.size() >= op.rx_size);
+			for(size_t i = 0; i < op.tx_size; i++)
+			{
+				txBuffer_.push_back(op.tx_buffer[i]);
+			}
+			for(size_t i = 0; i < op.rx_size; i++)
+			{
+				op.rx_buffer[i] = rxBuffer_.front();
+				rxBuffer_.pop();
+			}
+			break;
+		case embvm::i2c::operation::write:
 		case embvm::i2c::operation::writeNoStop:
 		case embvm::i2c::operation::continueWriteNoStop:
 		case embvm::i2c::operation::continueWriteStop:
-			bus_status_ = embvm::i2c::status::ok;
+			assert(op.tx_buffer);
+			assert(op.tx_size > 0);
+			for(size_t i = 0; i < op.tx_size; i++)
+			{
+				txBuffer_.push_back(op.tx_buffer[i]);
+			}
 			break;
 	}
 
-	return bus_status_;
+	//callback(op, embvm::i2c::status::ok, cb);
+
+	return embvm::i2c::status::ok;
 }
 
 embvm::i2c::pullups i2cTestDriver::setPullups_(embvm::i2c::pullups pullups) noexcept
