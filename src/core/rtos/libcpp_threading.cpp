cd9f245e6a85cad0911d19c6b4a229a5340d75a5
Phillip Johnston <phillip@embeddedartistry.com>
2022-01-07 08:59:09 -0800

    Questionable: apply clang-tidy fixes to libcpp_threading.cpp

--- src/core/rtos/libcpp_threading.cpp
+++ src/core/rtos/libcpp_threading.cpp
@@ -14,88 +14,88 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 
 #pragma mark - Recursive Mutex Functions -
 
-int __libcpp_recursive_mutex_lock(__libcpp_recursive_mutex_t* __m)
+auto __libcpp_recursive_mutex_lock(__libcpp_recursive_mutex_t* _m) -> int
 {
-	(*__m)->lock();
+	(*_m)->lock();
 	return 0;
 }
 
-bool __libcpp_recursive_mutex_trylock(__libcpp_recursive_mutex_t* __m)
+auto __libcpp_recursive_mutex_trylock(__libcpp_recursive_mutex_t* _m) -> bool
 {
-	return (*__m)->trylock();
+	return (*_m)->trylock();
 }
 
-int __libcpp_recursive_mutex_unlock(__libcpp_mutex_t* __m)
+auto __libcpp_recursive_mutex_unlock(__libcpp_mutex_t* _m) -> int
 {
-	(*__m)->unlock();
+	(*_m)->unlock();
 	return 0;
 }
 
-int __libcpp_mutex_lock(__libcpp_mutex_t* __m)
+auto __libcpp_mutex_lock(__libcpp_mutex_t* _m) -> int
 {
-	if((*__m) == nullptr)
+	if((*_m) == nullptr)
 	{
-		__libcpp_mutex_init(__m);
+		__libcpp_mutex_init(_m);
 	}
 
-	(*__m)->lock();
+	(*_m)->lock();
 
 	return 0;
 }
 
-bool __libcpp_mutex_trylock(__libcpp_mutex_t* __m)
+auto __libcpp_mutex_trylock(__libcpp_mutex_t* _m) -> bool
 {
-	if((*__m) == nullptr)
+	if((*_m) == nullptr)
 	{
-		__libcpp_mutex_init(__m);
+		__libcpp_mutex_init(_m);
 	}
 
-	return (*__m)->trylock();
+	return (*_m)->trylock();
 }
 
-int __libcpp_mutex_unlock(__libcpp_mutex_t* __m)
+auto __libcpp_mutex_unlock(__libcpp_mutex_t* _m) -> int
 {
-	if((*__m) == nullptr)
+	if((*_m) == nullptr)
 	{
-		__libcpp_mutex_init(__m);
+		__libcpp_mutex_init(_m);
 	}
 
-	(*__m)->unlock();
+	(*_m)->unlock();
 	return 0;
 }
 
 #pragma mark - Condition Variable -
 
-int __libcpp_condvar_signal(__libcpp_condvar_t* __cv)
+auto __libcpp_condvar_signal(__libcpp_condvar_t* _cv) -> int
 {
-	if((*__cv) == nullptr)
+	if((*_cv) == nullptr)
 	{
-		__libcpp_condvar_create(__cv);
+		__libcpp_condvar_create(_cv);
 	}
 
-	(*__cv)->signal();
+	(*_cv)->signal();
 	return 0;
 }
 
-int __libcpp_condvar_broadcast(__libcpp_condvar_t* __cv)
+auto __libcpp_condvar_broadcast(__libcpp_condvar_t* _cv) -> int
 {
-	if((*__cv) == nullptr)
+	if((*_cv) == nullptr)
 	{
-		__libcpp_condvar_create(__cv);
+		__libcpp_condvar_create(_cv);
 	}
 
-	(*__cv)->broadcast();
+	(*_cv)->broadcast();
 	return 0;
 }
 
-int __libcpp_condvar_wait(__libcpp_condvar_t* __cv, __libcpp_mutex_t* __m)
+auto __libcpp_condvar_wait(__libcpp_condvar_t* _cv, __libcpp_mutex_t* _m) -> int
 {
-	if((*__cv) == nullptr)
+	if((*_cv) == nullptr)
 	{
-		__libcpp_condvar_create(__cv);
+		__libcpp_condvar_create(_cv);
 	}
 
-	bool successful = (*__cv)->wait(*__m);
+	bool successful = (*_cv)->wait(*_m);
 
 	return successful ? 0 : -1;
 }
@@ -103,13 +103,13 @@ int __libcpp_condvar_wait(__libcpp_condvar_t* __cv, __libcpp_mutex_t* __m)
 #pragma mark - Generic Threading Functions -
 
 // Returns non-zero if the thread ids are equal, otherwise 0
-bool __libcpp_thread_id_equal(__libcpp_thread_id t1, __libcpp_thread_id t2)
+auto __libcpp_thread_id_equal(__libcpp_thread_id t1, __libcpp_thread_id t2) -> bool
 {
 	return t1 == t2;
 }
 
 // Returns non-zero if t1 < t2, otherwise 0
-bool __libcpp_thread_id_less(__libcpp_thread_id t1, __libcpp_thread_id t2)
+auto __libcpp_thread_id_less(__libcpp_thread_id t1, __libcpp_thread_id t2) -> bool
 {
 	return t1 < t2;
 }
@@ -119,24 +119,24 @@ void __libcpp_thread_yield()
 	embvm::this_thread::yield();
 }
 
-void __libcpp_thread_sleep_for(const chrono::nanoseconds& __ns)
+void __libcpp_thread_sleep_for(const chrono::nanoseconds& _ns)
 {
-	embvm::this_thread::sleep_for(__ns);
+	embvm::this_thread::sleep_for(_ns);
 }
 
-__libcpp_thread_id __libcpp_thread_get_current_id()
+auto __libcpp_thread_get_current_id() -> __libcpp_thread_id
 {
 	return embvm::this_thread::get_handle();
 }
 
-__libcpp_thread_id __libcpp_thread_get_id(const __libcpp_thread_t* __t)
+auto __libcpp_thread_get_id(const __libcpp_thread_t* _t) -> __libcpp_thread_id
 {
-	return (*__t)->native_handle();
+	return (*_t)->native_handle();
 }
 
-int __libcpp_thread_join(__libcpp_thread_t* __t)
+auto __libcpp_thread_join(__libcpp_thread_t* _t) -> int
 {
-	(*__t)->join();
+	(*_t)->join();
 	return 0;
 }
 
